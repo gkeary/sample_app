79855a582f8b9ce7ab23f5bb0d35baa0c6e31c79
Gregg Keary <gregg@gkeary.com>
2011-10-26 17:39:13 -0400

    pages controller spec now uses before(:each)
    	modified:   spec/controllers/pages_controller_spec.rb

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,6 +5,12 @@ describe PagesController do
   render_views
 
   describe "GET 'home'" do
+
+      
+    before(:each) do
+      @base_title =  "Ruby on Rails Tutorial Sample App"
+    end
+    
     it "should be successful" do
       get 'home'
       response.should be_success
@@ -13,7 +19,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-          :content => "Ruby on Rails Tutorial Sample App | Home")
+                                     :content => "#{@base_title} | Home")
     end
     
   end
@@ -27,7 +33,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-          :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                     :content => "#{@base_title} | Contact")
     end
   end
 
@@ -40,7 +46,8 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-          :content => "Ruby on Rails Tutorial Sample App | About")
+                                     :content => "#{@base_title} | About")
+     
     end
   end
 
