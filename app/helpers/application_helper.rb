4295817353347beee816d8b157ee36c63e5aae15
Gregg Keary <gregg@gkeary.com>
2011-11-15 08:10:33 -0500

    Ch4 finished.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # return a title on a per page basis
+  def title
+    base_title ="Ruby on Rails Sample App"
+    if @title.nil? == nil
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
