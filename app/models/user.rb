e3ad5787fa3ccc2ead9540f4fb21d4bcb3d6e0f2
Gregg Keary <gregg@gkeary.com>
2011-11-20 09:32:53 -0500

    password && encrypted password partially implemented
    about to execuet section 7.2
    password tests all green
    
    	modified:   app/models/user.rb
    	new file:   db/migrate/20111120141547_add_password_to_users.rb
    	modified:   db/schema.rb
    	modified:   spec/models/user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -21,4 +23,19 @@ class User < ActiveRecord::Base
             :format => {:with => email_regex } ,
             uniqueness: { case_sensitive: false}
 
+  validates :password, presence: true,
+                       confirmation: true,
+                       length: { within: 6..40 }
+
+  before_save :encrypt_password
+
+  private
+
+    def encrypt_password
+      self.encrypted_password= encrypt(password)
+    end
+
+    def encrypt(string)
+      string # Only a temporary implementation!
+    end
 end
