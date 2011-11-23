871256f0270a825d8ad610546a90f9120d1b627b
Gregg Keary <gregg@gkeary.com>
2011-11-23 09:23:45 -0500

    57 examples 0 failures 1 pending

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -33,5 +33,23 @@ describe "LayoutLinks" do
                                          content: "Sign in")
     end
   end
+
+  describe "when signed in" do
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :email,         with: @user.email
+      fill_in :password,      with: @user.password
+      click_button
+     end
+
+     it "should have a signout link" do
+       visit root_path
+       response.should have_selector("a", href: signout_path,
+                                         content: "Sign out")
+     end
+
+     it "should have a profile link"
+  end
 end 
 
