e4c9b8b005be70239666508b475ad94f5b7f32e0
Gregg Keary <gregg@gkeary.com>
2011-11-23 14:16:42 -0500

    removed redundant GET :new spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -46,18 +46,6 @@ describe UsersController do
     end
   end
 
-  describe "Get'new'" do
-    
-    it "should be successful" do
-      get :new
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get :new
-      response.should have_selector("title", content: 'Sign up')
-    end
-  end
   
   describe "POST 'create'" do
     describe "success" do
