918118fa064c33106765d375aeabd8c0ec89133d
Gregg Keary <gregg@gkeary.com>
2011-11-26 20:04:05 -0500

    Basic user authentication and page protection is complete.  71Examples 0 failures

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,6 +23,16 @@ module SessionsHelper
     self.current_user = nil
   end
 
+  def deny_access
+    store_location
+    redirect_to signin_path, notice: "Please sign in to access this page."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
   private
 
   def user_from_remember_token
@@ -37,8 +47,12 @@ module SessionsHelper
     user == current_user
   end
 
-  def deny_access
-    redirect_to signin_path, notice: "Please sign in to access this page."
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
   end
 
 end
