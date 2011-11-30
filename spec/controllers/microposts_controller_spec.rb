029905f37a011adc1dc3189ac0e3059e68267d81
Gregg Keary <gregg@gkeary.com>
2011-11-30 10:34:44 -0500

    fixed the bug def create should use params[:micropost] vice params(:micropost) !!!

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -16,55 +16,55 @@ describe MicropostsController do
     end
   end
 
-#  describe "POST 'create'" do
-#    
-#    before(:each) do
-#      @user = test_sign_in(Factory(:user))
-#    end
+  describe "POST 'create'" do
+    
+    before(:each) do
+      @user = test_sign_in(Factory(:user))
+    end
   
-#    describe "failure" do
-      
-#      before(:each) do
-#        @attr = { content: "" }
-#      end
+    describe "failure" do
+     
+      before(:each) do
+        @attr = { content: "" }
+      end
 
-#  ToDO:  5 tests do not work?
-# the next 5 tests do not work
-#      it "should not create a micropost" do
-#        lambda do
-#          post :create, micropost: @attr
-#        end.should_not change(Micropost, :count)
-#      end
-#
-#      it "should render the home page" do
-#        post :create, :micropost => @attr
-#        response.should render_template('pages/home')
-#      end
-#    end
-#  
-#    describe "success" do
-#      
-#      before(:each) do
-#        @attr = { :content => "Lorem ipsum" }
-#      end
-#      
-#      it "should create a micropost" do
-#        lambda do
-#          post :create, :micropost => @attr
-#        end.should change(Micropost, :count).by(1)
-#      end
-#      
-#      it "should redirect to the home page" do
-#        post :create, :micropost => @attr
-#        response.should redirect_to(root_path)
-#      end
-#      
-#      it "should have a flash message" do
-#        post :create, :micropost => @attr
-#        flash[:success].should =~ /micropost created/i
-#      end
-#    end
-#  end
+       #  ToDO:  5 tests do not work?
+       # the next 5 tests do not work
+      it "should not create a micropost" do
+        lambda do
+          post :create, micropost: @attr
+        end.should_not change(Micropost, :count)
+      end
+
+      it "should render the home page" do
+        post :create, :micropost => @attr
+        response.should render_template('pages/home')
+      end
+    end
+  
+    describe "success" do
+      
+      before(:each) do
+        @attr = { :content => "Lorem ipsum" }
+      end
+      
+      it "should create a micropost" do
+        lambda do
+          post :create, :micropost => @attr
+        end.should change(Micropost, :count).by(1)
+      end
+      
+      it "should redirect to the home page" do
+        post :create, :micropost => @attr
+        response.should redirect_to(root_path)
+      end
+      
+      it "should have a flash message" do
+        post :create, :micropost => @attr
+        flash[:success].should =~ /micropost created/i
+      end
+    end
+  end
   
   describe "DELETE 'destroy'" do
 
