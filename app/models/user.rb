ffaadb93237c68a13c7003a29953f6002d8f9c2c
Gregg Keary <gregg@gkeary.com>
2011-12-01 00:03:32 -0500

    113 examples 0 failures
    created relationships table, model and tests
    
    	new file:   app/models/relationship.rb
    	modified:   app/models/user.rb
    	new file:   db/migrate/20111201042150_create_relationships.rb
    	modified:   db/schema.rb
    	new file:   spec/models/relationship_spec.rb
    	modified:   spec/models/user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   has_many :microposts, dependent: :destroy 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+
 
 #Method	                        Purpose
 #micropost.user                  Return the User object associated with the micropost.
