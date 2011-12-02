69ab502aa89a69fc22c1e6d4352144b5c3e26361
Gregg Keary <gregg@gkeary.com>
2011-12-02 09:30:17 -0500

    Conclusion

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
     @title = 'Home'
     if signed_in?
       @micropost= Micropost.new 
+      # The home action with a paginated feed.
+      # Note that the paginate method actually reaches 
+      #   all the way into the Micropost model method 
+      #         in Listing 12.45, 
+      #         arranging to pull out only 30 microposts 
+      #          at a time from the database.
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
